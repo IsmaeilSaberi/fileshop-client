5112f8b434424602285f476069dbabd0d9fb30ff
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2023-03-17 00:39:06 -0700

    first setup. tailwind.css added to project and some initial configes created.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>فروشگاه فایل اسماعیل</title>
+        <link />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
